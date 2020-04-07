110361707e2b34d93c6d38fa2fce208a09cc8a95
Jeeraphat Wihokharn <jeeraphat.wi@gmail.com>
2020-04-07 17:27:35 +0700

    no message

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.json())
+
+const database = {
+    users: [
+        {
+            id: '01',
+            name: 'Jeerapat',
+            email: 'jeeraphat.wi@gmail.com',
+            password: 'Tawan_101010',
+            repo: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res)=> {
     res.send('this is working')
+})
+
+app.post('/signin', (req, res)=> { 
+    if(req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success')
+        } else {
+            res.status(400).json('error logging in')
+        }
 })
 
 app.listen(3000, () => {
